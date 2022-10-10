a507b749c79f9f05a17f54b46da8f66e4be53e43
AlexandrIvanov <alexandr.1996@list.ru>
2022-10-11 00:12:13 +0300

    Refactoring Post into a component

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import c from './Post.module.css'
+
+type PostType = {
+    message: string
+    likesCount: number
+}
+
+export const Post: React.FC<PostType> = (props) => {
+    return (
+        <div className={c.post}>
+            <img src="https://avatars.mds.yandex.net/i?id=85396d2aea45615e98718158618597eb-5906238-images-thumbs&n=13"
+                 alt="avatar"/>
+            {props.message}
+            <div>
+                <span>{props.likesCount} likes</span>
+            </div>
+        </div>
+    )
+}
