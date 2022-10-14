490aed1d887c46b95a12f1d7ffad2fee6d7fbf4f
AlexandrIvanov <alexandr.1996@list.ru>
2022-10-14 22:53:22 +0300

    Refactor Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import c from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
-        <div className={c.content}>
-            <div>
-                <img
-                    src="https://catherineasquithgallery.com/uploads/posts/2021-02/1613402911_12-p-bezhevii-kosmos-fon-16.jpg"
-                    alt="background"/>
-            </div>
-            <div>
-                avatar + description
-            </div>
+        <div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
