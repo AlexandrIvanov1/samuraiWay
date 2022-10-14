7d65ee37b1ab638677a69d8415d5757f90d72710
AlexandrIvanov <alexandr.1996@list.ru>
2022-10-14 19:12:00 +0300

    Add dialogs and messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,26 @@ import c from './Dialogs.module.css'
 
 export function Dialogs() {
     return (
-        <div className={c.content}>
-            Dialogs
+        <div className={c.dialogs}>
+            <div className={c.dialogsItem}>
+                <div className={c.item + ' ' + c.active}>
+                    Alina
+                </div>
+                <div className={c.item}>
+                    Kirill
+                </div>
+                <div className={c.item}>
+                    Denis
+                </div>
+                <div className={c.item}>
+                    Alex
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>Hello</div>
+                <div className={c.message}>How are you?</div>
+                <div className={c.message}>What's your name?</div>
+            </div>
         </div>
     );
-};
+}
